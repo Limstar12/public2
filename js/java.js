8e1f96b0aeec3c699b5bb1dd9d9e423fd7f672f2
Limstar12 <phji0326@gmail.com>
2022-05-01 19:33:03 +0900

    sec3 반응형 처리 중

--- js/java.js
+++ js/java.js
@@ -1,3 +1,31 @@
-window.onload(function(){
-    
-})
+window.onload=function(){
+    AOS.init();
+
+    var swiper = new Swiper("#mySwiper.swiper", {
+        //centeredSlides: true,
+        // autoplay: {
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: 1,
+        spaceBetween: 10,        
+        breakpoints: {
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+          },
+        },
+      });
+}
